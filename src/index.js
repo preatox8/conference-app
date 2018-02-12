e4fcd97a91e2c7e9e32c2284983b48886927d6f2
preatox8 <34518673+preatox8@users.noreply.github.com>
2018-02-12 22:52:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import { resolve } from "url";
 
 alert('Conférence App démarré !');
+
+/ Service TalkService
+var tkservice = require('./common/talk.service.js');
+
+tkservice.TalkService().forEach(element => {
+    console.log("Présentateur :", element.firstname + " " + element.lastname);
+});
