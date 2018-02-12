ce14fd261a9bc22d5dd7438464a6cebee4732c74
preatox8 <34518673+preatox8@users.noreply.github.com>
2018-02-12 22:55:00 +0100

    Update talk.service.js

--- src/common/talk.service.js
+++ src/common/talk.service.js
@@ -1,3 +1,31 @@
+module.exports = class TalkService { 
+    constructor() {
+        this.mapSpeakers = new Map();
+    }
+
+    findAllSpeakers() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(mapSpeakers);                
+                reject("Speakers map not found");
+            }, 2000);
+        });
+    }
+
+}
+
+const req = new XMLHttpRequest();
+rep.open('GET', 'http://localhost:3000/speakers', true);
+req.onreadystatechange = (aEvt) => {
+    if(req.readyState == 4) {
+        if(req.status == 200)
+            req.responseText;
+        else
+            throw "Erreur pendant le chargement de la page";
+    }
+};
+req.send(null);
+
 function findAllSpeakers(value) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -15,4 +43,7 @@ function findAllSpeakers(value) {
     })
 }
 
+
+    
+   
 
